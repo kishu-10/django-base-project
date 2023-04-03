040824d12368e22b30d24266a51515f1988338f4
Kishu Maharjan <kishu.maharjan@infodev.com.np>
2023-04-03 15:18:39 +0545

    fix: refactor

--- user/views.py
+++ user/views.py
@@ -14,6 +14,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from rest_framework_simplejwt.views import TokenObtainPairView
+from helpers.viewsets import CustomModelViewSet
 
 from user.serializers import CustomTokenObtainPairSerializer
 
@@ -33,37 +34,15 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 
 
 class MenuListView(ListAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
-
-
-class RoleCreateView(APIView):
-    def post(self, request, format=None):
-        serializer = RoleSerializer(data=request.data)
-        if serializer.is_valid():
-            obj = serializer.save()
-            # create_log
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class RoleListView(ListAPIView):
-    serializer_class = RoleSerializer
-    queryset = Role.objects.all().order_by("-id")
-    filter_backends = [filters.SearchFilter]
-    search_fields = ["name", "code"]
-
-
-class RoleDetailView(APIView):
-    def get(self, request, *args, **kwargs):
-        role = get_object_or_404(Role, pk=kwargs.get("id"))
-        serializer = RoleSerializer(role)
-        return Response(serializer.data, status=status.HTTP_200_OK)
-
-
-class MenuListView(ListAPIView):
     queryset = Menu.objects.filter(is_active=True).order_by("order_id")
     serializer_class = MenuSerializer
+
+
+class RoleViewSets(CustomModelViewSet):
+    serializer_class = RoleSerializer
+    queryset = Role.objects.all()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ["name", "code"]
 
 
 class PrivilegeListView(ListAPIView):
