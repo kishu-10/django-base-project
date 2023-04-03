1b9e538417d57ec83b04aa50bf96c40e3a627618
Kishu Maharjan <kishu.maharjan@infodev.com.np>
2023-04-03 16:10:30 +0545

    fix: refactoring codes

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RoleViewSet
+from .views import RoleViewSet, InitAPI
 
 router = DefaultRouter(trailing_slash=False)
 router.register("role", RoleViewSet, basename="role-CRUD")
 
 urlpatterns = [
     path("", include(router.urls)),
+    path("init", InitAPI.as_view(), name="init_api"),
 ]
